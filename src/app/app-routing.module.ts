4a5187d5f68fa81145ea8c4f8d8d5d16ec3b259d
jsanc160 <jsanc160@ALICA071>
2024-09-30 12:59:53 +0200

    seguimos updateando el project :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeaderCabeceraComponent } from './header-cabecera/header-cabecera.component';
 import { HeaderContenidoComponent } from './header-contenido/header-contenido.component';
+import { SpecificPredictionsComponent } from './specific-predictions/specific-predictions.component';
 const routes: Routes = [
   { path: '', redirectTo: 'app-root', pathMatch: 'full' },
   { path: 'header', component: HeaderComponent, pathMatch: 'full' },
   { path: 'header-cabecera', component: HeaderCabeceraComponent, pathMatch: 'full' },
   { path: 'header-contenido', component: HeaderContenidoComponent, pathMatch: 'full' },
+  { path: 'specific-predictions', component: SpecificPredictionsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './home/home.component';
 import { ClarityModule } from '@clr/angular';
 import { HeaderCabeceraComponent } from './header-cabecera/header-cabecera.component';
 import { HeaderContenidoComponent } from './header-contenido/header-contenido.component';
+import { SpecificPredictionsComponent } from './specific-predictions/specific-predictions.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,12 @@ import { HeaderContenidoComponent } from './header-contenido/header-contenido.co
     HeaderComponent,
     HomeComponent,
     HeaderCabeceraComponent,
-    HeaderContenidoComponent
+    HeaderContenidoComponent,
   ],
   imports: [
     BrowserModule,
     ClarityModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
